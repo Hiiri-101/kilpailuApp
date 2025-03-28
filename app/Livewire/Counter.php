0645d8b8775214502d766454ce17788db47575ee
Hiiri-101 <fbiagentnumbor2@gmail.com>
2025-03-28 14:59:41 +0200

    livewire testing

--- app/Livewire/Counter.php
+++ app/Livewire/Counter.php
@@ -3,11 +3,41 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class Counter extends Component
 {
+    #[Locked]
+    public $count = 1;
+
+    public function increment()
+    {
+        $this->authorize('increment', $count);
+        $this->count++;
+    }
+
+    public function decrement()
+    {
+        $this->authorize('decrement', $count);
+        $this->count--;
+    }
+
     public function render()
     {
         return view('livewire.counter');
     }
 }
+
+/*
+class AppServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        Relation::morphMap([
+            'post' => 'App/'
+        ]);
+    }
+
+}
+*/
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,18 @@
 <div>
+    <h1>{{ $count }}</h1>
 
+    <button wire:click="increment">+</button>
+
+    <button wire:click="decrement">-</button>
+
+    @php
+    $listing = ['item1','item2','item3']
+    @endphp
+
+    @foreach ($listing as $item)
+        @if ($item = 'item2')
+           <p>{{"found"}}</p>
+        @endif
+
+    @endforeach
 </div>
